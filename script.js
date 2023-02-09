9615ca4e0598c8a66ad37ed33f22643f6f819c5d
gabaeliz <elizabethg.eg12@gmail.com>
2023-02-08 22:37:39 -0600

    Se agregaron los cambios al CSS

---
+++ script.js
@@ -0,0 +1,33 @@
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
+//Queremos saber cuando el usuario le esta dando clic al boton de agregar
+(() => {
+    const btn = document.querySelector('[data-form-btn]');
+
+    const createTask = (evento) => {
+        evento.preventDefault();
+        const input = document.querySelector('[data-form-input]');
+        const value = input.value;
+        const list = document.querySelector('[data-list]');
+        const task = document.createElement('li');
+        task.classList.add('card');
+        input.value = '';
+        const taskContent = document.createElement('div');
+        
+        const titleTask = document.createElement('span');
+        titleTask.classList.add('task');
+        titleTask.innerText = value;
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(titleTask);
+
+        //task.innerHTML = content;
+        //Al elemento lista le quiero agregar un "hijo"(tarea)
+        task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
+        list.appendChild(task);
+    };
+    //Arrow functions o funciones anonimas 
+    btn.addEventListener('click', createTask);
+
+})();
